fe1403550089f95f5c1a4756199f0031527afa35
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2021-12-26 16:33:29 -0300

    feat: created validate token function

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -23,4 +23,23 @@ module.exports = {
 
     next();
   },
+  async validateToken(request, response, next){
+    
+    const { cookie } = request.params;
+    
+    if(!cookie) return response.status(401).json({ error: "No token provided"});
+    
+    const parts = cookie?.split(' ')
+    const [ scheme, token ] = parts || ""
+    
+    if(!parts.lenght === 2 || !/^Bearer$/i.test(scheme)) return response.status(401).json({ error: "Token isn't valid"})
+    
+
+    jwt.verify(token, authConfig.secret, (error, decoded) => {
+      if(error) return response.status(401).json({ error: "Token invalid"})
+      
+      request.userID = decoded.id
+      return next()
+    })
+  }
 };
