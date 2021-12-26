a03aefa152226179d712e03748f0708e83efa66c
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2021-12-26 16:33:19 -0300

    feat: created validate id function

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -4,4 +4,23 @@ const jwt = require("jsonwebtoken")
 const authConfig = require("../configs/auth.js")
 
 module.exports = {
+  async validateId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+      return response.status(400).json({ error: "Invalid ID" });
+    }
+
+    try {
+      const user = await Users.findById(id);
+      response.user = user;
+      if (!user) {
+        return response.status(404).json({ error: "User not found" });
+      }
+    } catch (error) {
+      return response.status(500).json({ error: error.message });
+    }
+
+    next();
+  },
 };
