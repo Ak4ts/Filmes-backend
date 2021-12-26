2a21bf270b3aeb68abe22cd4771b29a45968f554
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2021-12-26 16:31:00 -0300

    feat: created register function

--- src/controllers/UsersControllers/index.js
+++ src/controllers/UsersControllers/index.js
@@ -4,4 +4,19 @@ const jwt = require("jsonwebtoken");
 const authConfig = require("../../configs/auth");
 
 module.exports = {
+  async register(request, response) {
+    const { username, email, password, phone } = request.body;
+    if ( !username || !password || !email || !phone ) {
+      return response.status(400).json({ error: "Cannot continue with blank fields." });
+    }
+
+    const user = new Users({ _id: uuid(), username, email, password, phone });
+
+    try {
+      await user.save();
+      return response.status(201).json({ message: "User created succesfully!!" });
+    } catch (error) {
+      response.status(400).json({ error: "Bad request" });
+    }
+  },
 };
