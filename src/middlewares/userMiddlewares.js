449706c5944f6868b254e9eac18b2861eb2af7de
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2021-12-26 16:32:12 -0300

    feat: created users middleware function

---
+++ src/middlewares/userMiddlewares.js
@@ -0,0 +1,7 @@
+const { validate: isUuid } = require("uuid");
+const Users = require("../models/Users");
+const jwt = require("jsonwebtoken")
+const authConfig = require("../configs/auth.js")
+
+module.exports = {
+};
