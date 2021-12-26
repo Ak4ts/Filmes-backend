f0aef8416a42c7c53a11e6c409da38a5369826c5
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2021-12-26 16:29:53 -0300

    feat: created users controllers

---
+++ src/controllers/UsersControllers/index.js
@@ -0,0 +1,7 @@
+const Users = require("../../models/Users");
+const { v4: uuid } = require("uuid");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../../configs/auth");
+
+module.exports = {
+};
